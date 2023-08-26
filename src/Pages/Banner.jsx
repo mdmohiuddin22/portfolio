eef39ecfd883430b9d4fb818fc905c3f18fbc17b
Md.Mohiuddin <saddammd203@gmail.com>
2023-08-26 23:55:56 +0600

    add second line

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -4,6 +4,8 @@ const Banner = () => {
     return (
     <div>
       <h1 className="text-3xl font-bold mt-28">Hello, my name is <span className="text-amber-500">Md.Mohiuddin</span>  </h1>
+      <h1 className="text-3xl font-bold mt-16">I' m a <span className="text-amber-500">Mern-Stack Developer</span> </h1>
+      <p>I am a well-versed MERN Stack developer & designer with 1 years plus experience. I have led many projects from design to deployment which have impacted thousands of people. I love to blend my skills in the most creative way possible.</p>
     </div>
     );
 };
